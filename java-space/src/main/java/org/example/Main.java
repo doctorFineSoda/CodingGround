0465ca21c6f5f67acb40a1daf257db1f71912eb6
vidana.popkova <125513175+doctorFineSoda@users.noreply.github.com>
2023-02-16 23:05:42 +0100

    Add java project skeleton
    ## Summary
    - add basic java project structure for future scripts
    - add gitignore for keeping the project clean

---
+++ java-space/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package main.java.org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Task1.foo();
+    }
+}
---
+++ java-space/src/main/java/org/example/Task1.java
@@ -0,0 +1,10 @@
+package main.java.org.example;
+
+public class Task1 {
+
+    // implementation of first task
+    public static void foo() {
+        System.out.println("Solution for task 1");
+    }
+
+}
