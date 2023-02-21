8433621774eec664a1aac6606166c137172146eb
vidana.popkova <125513175+doctorFineSoda@users.noreply.github.com>
2023-02-21 13:37:09 +0100

    solved leetCode Problem

--- go-space/main.go
+++ go-space/main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world! :)")
+	task4()
+}
+
+func task4() {
+	numbs := []int{1, 1, 2, 3, 3, 4, 4, 8, 8}
+	fmt.Printf("Element: %d\n", singleNonDuplicate(numbs))
 }
